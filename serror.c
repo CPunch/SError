d2f3f2cab1d4a2c5fa4265d3ac2e7ef5f3b2d09f
CPunch <sethtstubbs@gmail.com>
2022-04-25 23:55:24 -0500

    Update serror.c

--- serror.c
+++ serror.c
@@ -1,9 +1,9 @@
 #include "serror.h"
 
 #ifdef __USE_ISOC11
-thread_local int __eSimple_errIndx;
+thread_local int __eSimple_errIndx = -1;
 thread_local jmp_buf __eSimple_errStack[SIMPLE_MAXERRORS];
 #else
-int __eSimple_errIndx;
+int __eSimple_errIndx = -1;
 jmp_buf __eSimple_errStack[SIMPLE_MAXERRORS];
 #endif
